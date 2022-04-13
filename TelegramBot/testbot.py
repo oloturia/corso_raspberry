dee252b981f18e7bf66e1c004e49a6e3a400688a
Emattioli <5429234+oloturia@users.noreply.github.com>
2022-04-13 11:34:08 +0200

    Update testbot.py
    fixed exposed token

--- TelegramBot/testbot.py
+++ TelegramBot/testbot.py
@@ -5,7 +5,11 @@ from telegram.ext import Updater, CommandHandler, CallbackContext
 def hello(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(f'hello {update.effective_user.first_name}')
 
-updater = Updater('5275467894:AAGauowka-sQu5X-GKCLlfwTrWKvTZlHFRg')
+with open("TOKEN") as token_file:
+    token_lines = token_file.readlines()
+    token = token_lines[0].strip()
+    
+updater = Updater(token)
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
 updater.start_polling()
 updater.idle()
